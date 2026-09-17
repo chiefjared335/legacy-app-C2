b35a7092cb8fea04d10d33c82af33ae73660806c
Berkay0164 <131471878+Berkay0164@users.noreply.github.com>
2026-09-17 12:18:52 +0200

    issue485 klaar
    Bart include in webdev en ine met verlof

--- PRA_c2_4s-main/includes/collegas/ine.php
+++ PRA_c2_4s-main/includes/collegas/ine.php
@@ -1,6 +1,6 @@
 <div class="collega">
     <img src="img/profile_pictures/ib81.jfif" alt="ib81">
     <h4>Ine Vermeer</h4>
-    <p>docent WEB/SLB</p>
+    <p>docent WEB/SLB (met verlof t/m april)</p>
     <p><a href="mailto:e.vermeer@curio.nl">e.vermeer@curio.nl</a></p>
 </div>
